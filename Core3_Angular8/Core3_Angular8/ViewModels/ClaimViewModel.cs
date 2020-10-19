680a0a6302b57d1554bab35a4363bfcc86d90c2b
Ramanagoudap <patil.ramanagouda@gmail.com>
2020-10-19 21:07:26 +0530

    Add project files.

---
+++ Core3_Angular8/Core3_Angular8/ViewModels/ClaimViewModel.cs
@@ -0,0 +1,16 @@
+﻿// =============================
+// Email: info@ebenmonney.com
+// www.ebenmonney.com/templates
+// =============================
+
+using System;
+using System.Linq;
+
+namespace Core3_Angular8.ViewModels
+{
+    public class ClaimViewModel
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+}
